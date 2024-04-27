326510e8a9ed1f934bfb1f8513409f207ad9bd26
zubayer14730 <kadir15-14730@diu.edu.bd>
2024-04-27 20:26:59 +0600

    portfolio

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Zubayer Al Kadir, a Flutter developer, Technical blog writer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I\'m student of Daffodil International Univarsity. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience.';
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:folio/sections/about/about.dart';
+import 'package:folio/sections/contact/contact.dart';
+import 'package:folio/sections/home/home.dart';
+import 'package:folio/sections/portfolio/portfolio.dart';
+import 'package:folio/sections/services/services.dart';
+import 'package:folio/widget/footer.dart';
+
+class StaticUtils {
+  static const String hi = 'assets/hi.gif';
+
+  // photos
+  static const String mobilePhoto = 'assets/photos/mobile.png';
+  static const String coloredPhoto = 'assets/photos/colored.png';
+  static const String blackWhitePhoto = 'assets/photos/black-white.png';
+
+  // work
+  static const String dsc = 'assets/work/dsc.png';
+  static const String sastaticket = 'assets/work/st.png';
+  static const String university = 'assets/work/cui.png';
+  static const String fullterIsb = 'assets/work/flutterIsl.png';
+
+  // services
+  static const String uiux = 'assets/services/ui.png';
+  static const String blog = 'assets/services/blog.png';
+  static const String appDev = 'assets/services/app.png';
+  static const String rapid = 'assets/services/rapid.png';
+  static const String openSource = 'assets/services/open.png';
+  static const String openSourceBlack = 'assets/services/open_b.png';
+
+  static const List<String> socialIconURL = [];
+
+  static const List<String> socialLinks = [];
+
+  static const String resume = 'https://github.com/zubayer14730';
+
+  static const String gitHub = 'https://github.com/zubayer14730';
+}
+
+class BodyUtils {
+  static const List<Widget> views = [
+    HomePage(),
+    About(),
+    Services(),
+    Portfolio(),
+    Contact(),
+    Footer(),
+  ];
+}
